eadbdfce5da88191949d1fd7d5263c19887440e7
Rachhen <rachhen.it@outlook.com>
2024-03-04 22:35:18 +0700

    first commit

---
+++ db/sqlc/entry_test.go
@@ -0,0 +1,98 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"testing"
+	"time"
+
+	"github.com/rachhen/simple_bank/util"
+	"github.com/stretchr/testify/require"
+)
+
+func createRandomEntry(t *testing.T) Entry {
+	account := createRandomAccount(t)
+
+	arg := CreateEntryParams{
+		AccountID: account.ID,
+		Amount:    util.RandomMoney(),
+	}
+
+	entry, err := testQueries.CreateEntry(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry)
+
+	require.Equal(t, entry.AccountID, arg.AccountID)
+	require.Equal(t, entry.Amount, arg.Amount)
+
+	require.NotZero(t, entry.ID)
+	require.NotZero(t, entry.CreatedAt)
+
+	return entry
+}
+
+func TestCreateEntry(t *testing.T) {
+	createRandomEntry(t)
+}
+
+func TestGetEntry(t *testing.T) {
+	entry := createRandomEntry(t)
+
+	entry2, err := testQueries.GetEntry(context.Background(), entry.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry2)
+
+	require.Equal(t, entry.ID, entry2.ID)
+	require.Equal(t, entry.AccountID, entry2.AccountID)
+	require.Equal(t, entry.Amount, entry2.Amount)
+	require.WithinDuration(t, entry.CreatedAt, entry2.CreatedAt, time.Second)
+}
+
+func TestUpdateEntry(t *testing.T) {
+	entry := createRandomEntry(t)
+
+	arg := UpdateEntryParams{
+		ID:     entry.ID,
+		Amount: util.RandomMoney(),
+	}
+
+	entry2, err := testQueries.UpdateEntry(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry2)
+
+	require.Equal(t, entry.ID, entry2.ID)
+	require.Equal(t, entry.AccountID, entry2.AccountID)
+	require.Equal(t, arg.Amount, entry2.Amount)
+	require.WithinDuration(t, entry.CreatedAt, entry.CreatedAt, time.Second)
+}
+
+func TestDeleteEntry(t *testing.T) {
+	entry := createRandomEntry(t)
+
+	err := testQueries.DeleteEntry(context.Background(), entry.ID)
+	require.NoError(t, err)
+
+	entry2, err := testQueries.GetEntry(context.Background(), entry.ID)
+	require.Error(t, err)
+	require.EqualError(t, err, sql.ErrNoRows.Error())
+	require.Empty(t, entry2)
+}
+
+func TestListEntries(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		createRandomEntry(t)
+	}
+
+	arg := ListEntriesParams{
+		Limit:  5,
+		Offset: 5,
+	}
+
+	entries, err := testQueries.ListEntries(context.Background(), arg)
+	require.NoError(t, err)
+	require.Len(t, entries, 5)
+
+	for _, entry := range entries {
+		require.NotEmpty(t, entry)
+	}
+}
---
+++ util/random.go
@@ -0,0 +1,49 @@
+package util
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+
+}
+
+// RandomInt generates a random integer between min and max
+func RandomInt(min, max int) int {
+	return min + rand.Intn(max-min)
+}
+
+// RandomString generates a random string of length n
+func RandomString(n int) string {
+	var sb strings.Builder
+	l := len(alphabet)
+
+	for i := 0; i < n; i++ {
+		c := alphabet[rand.Intn(l)]
+		sb.WriteByte(c)
+	}
+
+	return sb.String()
+}
+
+// RandomOwner generates a random owner name
+func RandomOwner() string {
+	return RandomString(6)
+}
+
+// RandomMoney generates a random amount of money
+func RandomMoney() int64 {
+	return int64(RandomInt(0, 1000))
+}
+
+// RandomCurrency generates a random currency code
+func RandomCurrency() string {
+	currencies := []string{"USD", "EUR", "CAD"}
+	n := len(currencies)
+	return currencies[rand.Intn(n)]
+}
