5cf1352e1181ad9ef571cfc9fe8ccce9bbebe423
Rachhen <rachhen.it@outlook.com>
2024-03-05 16:11:58 +0700

    add transfer and custom currency validator

---
+++ util/currency.go
@@ -0,0 +1,18 @@
+package util
+
+// Constants for all supported currencies
+const (
+	USD = "USED"
+	EUR = "EUR"
+	CAD = "CAD"
+)
+
+// IsSupportedCurrency return true if the currency is supported
+func IsSupportedCurrency(currency string) bool {
+	switch currency {
+	case USD, EUR, CAD:
+		return true
+	}
+
+	return false
+}
